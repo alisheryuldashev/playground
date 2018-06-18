41832257f9adf3debd3a61047c754eba06afceec
Alisher Yuldashev <alyuldashev@yahoo.ca>
2018-06-18 08:47:25 -0600

    Update hello world.py

--- Python/hello world.py
+++ Python/hello world.py
@@ -1,6 +1,7 @@
 #this snippet should prompt for your name and return a string: "Hello, [your name]"
 
-#import custom functions from CS50 library used in Harvard Introduction to Computer Science course. from cs50 import get_string 
+#import custom functions from CS50 library used in Harvard Introduction to Computer Science course. 
+from cs50 import get_string 
 
 s = get_string("name: ")
 print("hello, {}".format(s))
