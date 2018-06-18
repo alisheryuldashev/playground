674df34f4409beac4737c44d12295619610854e9
Alisher Yuldashev <alyuldashev@yahoo.ca>
2018-06-18 09:22:14 -0600

    Update char.py

--- Python/char.py
+++ Python/char.py
@@ -1,5 +1,5 @@
 #this snippet will take an argument and print each character of that string an a new line
-#(example: python char.py command will print c h a r . p  y with every character displayting on a new line) 
+#(example: python char.py command will print c h a r . p  y with every character output on a new line) 
 
 import sys
 
